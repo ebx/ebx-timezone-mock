0b14b7e651257a79d040f7d46c815c47e83efaaa
Andy Neale <andy@echobox.com>
2018-05-15 17:21:23 +0100

    Recompiled TimezoneMock.js

--- TimezoneMock.js
+++ TimezoneMock.js
@@ -172,6 +172,8 @@ function _interopRequireWildcard(obj) {
         this.date = new _Date(y, m, d, h, M, s, ms);
         this.fromLocal(new _Date(_Date.UTC.apply(null, arguments)));
     }
+
+    return this;
   }
 
   // Returns the timezone offset for any given date/time in the current timezone
@@ -255,6 +257,8 @@ function _interopRequireWildcard(obj) {
       var date = new _Date(this.date.getTime() - this._tzOffset() * HOUR);
       date['setUTC' + fn.slice(3)].apply(date, arguments);
       this.fromLocal(date);
+
+      return this;
     };
   }
 
